5c869b88ff10aca7abd1ab8affa20c8612f0f707
Mudassir-13 <mbaksh@hawk.iit.edu>
2023-11-16 17:57:53 -0600

    script.js
    adding more java script

--- script.js
+++ script.js
@@ -3,6 +3,29 @@ function initMap() {
       zoom: 14,
       center: {lat: 41.77733255084747, lng: -87.61660508659033},
     });
+    var marker = new google.maps.Marker({
+      position: {lat: 41.77733255084747, lng: -87.61660508659033}, 
+      map: map,
+      icon: "images/von.png",
+      title: 'Parkway Gardens',
+      animation: google.maps.Animation.DROP,
+      draggable: true,
+    });
+    
+    var infoWindow = new google.maps.InfoWindow({
+      content: '<div style="width: 200px; height: 150px;">' +
+        '<h3>Parkway Gardens</h3>' +
+        '<p>Parkway Gardens, in Chicago IL is a lively and positive community.</p>' +
+        '<p>It has lots of green spaces, a friendly vibe, and people here feel connected.</p>' +
+        '<p>The neighborhood truly captures the essence of city living.</p>' +
+        '<p>Residents love the mix of cultures, local amenities, and the welcoming community, making every day a happy experience.</p>' +
+        '</div>'
+    });
+    
+    marker.addListener('click', function() {
+      infoWindow.open(map, marker);
+    });
+    
   
     var marker = new google.maps.Marker({
       position: {lat: 41.77733255084747, lng: -87.61660508659033}, 
@@ -10,4 +33,3 @@ function initMap() {
       title: 'Parkway Gardens',
     });
   }
-  
